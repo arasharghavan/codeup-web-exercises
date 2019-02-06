628fc725d470dc73fc32043577daa4a493f187df
Arash <arash.arghavan@gmail.com>
2019-02-06 16:34:40 -0600

    new file

---
+++ js/external.js
@@ -0,0 +1,43 @@
+"use strict"
+
+
+
+var little = prompt("how many days for little mermaid?");
+var Bear = prompt("how many days for brother bear?");
+var Hercules = prompt("how many days for Hercules?");
+
+var days = Number(little) + Number(Bear) + Number(Hercules);
+
+var total = days * 3;
+
+alert("your total amount is" + total + " and your total days are " + days);
+
+
+
+var google = prompt("how many hours did you worked for google?");
+var googlem = Number(google) * 400;
+
+var amazon = prompt("how many hours did you worked for amazon?");
+var amazonm = Number(amazon) * 380;
+
+var face = prompt("how many hours did you worked for facebook?");
+var facem = Number(face) * 350;
+
+
+var income = googlem + facem + amazonm;
+
+ alert("your total income is " + income );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
